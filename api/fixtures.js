162665f23a047e02571821ffde49cce5cac3a89c
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-04-08 23:25:47 +0600

    Added fixtures.

--- api/fixtures.js
+++ api/fixtures.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const {nanoid} = require('nanoid');
 const config = require('./config');
 const User = require('./models/User');
+const Cocktail = require('./models/Cocktail');
 
 const run = async () => {
   await mongoose.connect(config.mongo.db, config.mongo.options);
@@ -12,7 +13,7 @@ const run = async () => {
     await mongoose.connection.db.dropCollection(coll.name);
   }
 
-  await User.create(
+  const [User1, Admin] = await User.create(
     {
       email: 'user@user',
       password: '123',
@@ -30,6 +31,31 @@ const run = async () => {
       token: nanoid(),
     },
   );
+
+  await Cocktail.create(
+    {
+      user: User1,
+      title: 'Cocktail1',
+      image: 'fixtures/cocktail1.jpg',
+      recipe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolor dolorum error nemo quos ratione recusandae reprehenderit tenetur. Molestias, temporibus?',
+      is_published: false,
+      ingredients: [{ingTitle: 'Ing1', ingAmount: 'Amount1'}, {ingTitle: 'Ing2', ingAmount: 'Amount2'}],
+    },{
+      user: Admin,
+      title: 'Cocktail2',
+      image: 'fixtures/cocktail2.jpeg',
+      recipe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolor dolorum error nemo quos ratione recusandae reprehenderit tenetur. Molestias, temporibus?onsectetur adipisicing elit. Consectetur dolor dolorum error nemo quos ratione recusandae reprehenderit tenetur. Molestias, temporibus?',
+      is_published: true,
+      ingredients: [{ingTitle: 'Ing1', ingAmount: 'Amount1'}, {ingTitle: 'Ing2', ingAmount: 'Amount2'}],
+    },{
+      user: Admin,
+      title: 'Cocktail3',
+      image: 'fixtures/cocktail3.webp',
+      recipe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolor dolorum.',
+      is_published: true,
+      ingredients: [{ingTitle: 'Ing1', ingAmount: 'Amount1'}, {ingTitle: 'Ing2', ingAmount: 'Amount2'}],
+    },
+  )
 
   await mongoose.connection.close();
 };
